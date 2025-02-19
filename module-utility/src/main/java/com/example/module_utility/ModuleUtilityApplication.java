1182d6dab55ddf8a08ea2820ec1346ec81637c69
cie10 <leesky0075@dgu.ac.kr>
2025-02-19 15:26:07 +0900

    feat: Implement BaseEntity and Swagger configuration in module-utility(#4)

---
+++ module-utility/src/main/java/com/example/module_utility/ModuleUtilityApplication.java
@@ -0,0 +1,13 @@
+package com.example.module_utility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleUtilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuleUtilityApplication.class, args);
+	}
+
+}
---
+++ module-utility/src/test/java/com/example/module_utility/ModuleUtilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.module_utility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleUtilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
