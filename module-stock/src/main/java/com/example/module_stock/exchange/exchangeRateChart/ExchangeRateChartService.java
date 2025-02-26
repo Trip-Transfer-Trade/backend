e37a7a24ffe07fd5309074e0d1ea5514618d2e22
jeongIN <shjin4667@gmail.com>
2025-02-26 12:59:24 +0900

    feat: Add Redis caching for exchange rate chart (#22)

---
+++ module-stock/src/main/java/com/example/module_stock/exchange/exchangeRateChart/ExchangeRateChartService.java
@@ -0,0 +1,145 @@
+package com.example.module_stock.exchange.exchangeRateChart;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+
+@Service
+public class ExchangeRateChartService {
+    private static final Logger logger = LoggerFactory.getLogger(ExchangeRateChartService.class);
+
+    private final RestTemplate restTemplate;
+    private final ObjectMapper objectMapper;
+    private final RedisTemplate<String, String> redisTemplate;
+
+    @Value("${AUTH_KEY}")
+    private String authKey;
+
+    public ExchangeRateChartService(RestTemplate restTemplate, ObjectMapper objectMapper, RedisTemplate<String, String> redisTemplate) {
+        this.restTemplate = restTemplate;
+        this.objectMapper = objectMapper;
+        this.redisTemplate = redisTemplate;
+    }
+
+    public void saveExchangeRateChart() {
+        String today = getDate(0);
+        String lastDay = getDate(365);
+
+        String url = "https://www.koreaexim.go.kr/site/program/financial/exchangeJSON";
+        String queryParam = "?authkey=" + authKey
+                + "&searchdate=" + today
+                + "&data=AP01";
+
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        ResponseEntity<String> response = restTemplate.exchange(url + queryParam, HttpMethod.GET, entity, String.class);
+
+        logger.info("한국수출입은행 API 응답: {}", response.getBody());
+
+        try{
+            List<ExchangeRateSaveDTO> rates = objectMapper.readValue(response.getBody(), new TypeReference<List<ExchangeRateSaveDTO>>() {});
+            ZSetOperations<String, String> zSetOperations = redisTemplate.opsForZSet();
+
+            for(ExchangeRateSaveDTO rate : rates){
+                String code = rate.getCode();
+                String exchangeRate = rate.getExchangeRate();
+
+                String cacheKey = "exchangeRate:" + code;
+                String redisValue = today + ":" + exchangeRate;
+
+                zSetOperations.add(cacheKey, redisValue, Double.parseDouble(today));
+
+                zSetOperations.removeRangeByScore(cacheKey, 0, Double.parseDouble(lastDay));
+
+                logger.info("저장 완료");
+            }
+
+        } catch (Exception e) {
+            logger.error("환율 데이터 저장 오류");
+        }
+    }
+
+    public ExchangeRateChartDTO getExchangeRateChart(String code, int days) {
+        ZSetOperations<String, String> zSetOperations = redisTemplate.opsForZSet();
+
+        String cacheKey = "exchangeRate:" + code;
+        List<ExchangeRateChartDTO.ExchangeRateData> exchangeRateDataList = new ArrayList<>();
+        String lastRate = null;
+
+        for(int i = 0; i < days; i++){
+            String date = getDate(i);
+            Set<String> rateData = zSetOperations.rangeByScore(cacheKey, Double.parseDouble(date), Double.parseDouble(date));
+
+            if(rateData == null || rateData.isEmpty()){
+                if(lastRate != null){
+                    exchangeRateDataList.add(new ExchangeRateChartDTO.ExchangeRateData(date, lastRate));
+                }
+            } else {
+                String[] parts = rateData.iterator().next().split(":");
+                exchangeRateDataList.add(new ExchangeRateChartDTO.ExchangeRateData(parts[0], parts[1]));
+            }
+        }
+
+        return new ExchangeRateChartDTO(code, exchangeRateDataList);
+    }
+
+    // 1년 환율 데이터 저장 함수 -> 초기 실행 후 사용 안 함
+    public void saveTestData() {
+        ZSetOperations<String, String> zSetOperations = redisTemplate.opsForZSet();
+
+        for (int i = 0; i < 365; i++) {
+            String date = getDate(i);
+            String lastDay = getDate(365);
+
+            String url = "https://www.koreaexim.go.kr/site/program/financial/exchangeJSON";
+            String queryParam = "?authkey=" + authKey + "&searchdate=" + date + "&data=AP01";
+
+            HttpHeaders headers = new HttpHeaders();
+            HttpEntity<String> entity = new HttpEntity<>(headers);
+            ResponseEntity<String> response = restTemplate.exchange(url + queryParam, HttpMethod.GET, entity, String.class);
+
+            logger.info("한국수출입은행 API 응답: {}", response.getBody());
+
+            try {
+                List<ExchangeRateSaveDTO> rates = objectMapper.readValue(response.getBody(), new TypeReference<List<ExchangeRateSaveDTO>>() {});
+
+                for (ExchangeRateSaveDTO rate : rates) {
+                    String code = rate.getCode();
+                    String exchangeRate = rate.getExchangeRate();
+
+                    String cacheKey = "exchangeRate:" + code;
+                    String redisValue = date + ":" + exchangeRate;
+
+                    zSetOperations.add(cacheKey, redisValue, Double.parseDouble(date));
+
+                    zSetOperations.removeRangeByScore(cacheKey, 0, Double.parseDouble(lastDay));
+                }
+
+                logger.info("{} 날짜의 환율 데이터 저장 완료", date);
+            } catch (Exception e) {
+                logger.error("{} 날짜의 환율 데이터 저장 오류", date, e);
+            }
+        }
+    }
+
+    private String getDate(int daysAgo){
+        return LocalDate.now().minusDays(daysAgo).format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+    }
+}
