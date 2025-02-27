bac46fe181ef32ec97ec5382f57b8107eb63c442
0321minji <0321minji@ewhain.net>
2025-02-27 13:47:08 +0900

    git commit -m "feat : create account api for transaction(#31)"

--- module-trip/src/main/java/com/example/module_trip/account/AccountRepository.java
+++ module-trip/src/main/java/com/example/module_trip/account/AccountRepository.java
@@ -4,10 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Integer> {
 
     List<Account> findByUserId(Integer userId);
 
+    Optional<Account> findByAccountNumber(String accountNumber);
 }
