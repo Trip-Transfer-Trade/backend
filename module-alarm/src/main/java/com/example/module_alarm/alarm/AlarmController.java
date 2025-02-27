712c6e20477ecf3c3ca52926b11dd743c28a46e3
0321minji <0321minji@ewhain.net>
2025-02-27 15:12:28 +0900

    git commit -m "chore : modify response & alarm content(#7)"

--- module-alarm/src/main/java/com/example/module_alarm/alarm/AlarmController.java
+++ module-alarm/src/main/java/com/example/module_alarm/alarm/AlarmController.java
@@ -2,6 +2,7 @@ package com.example.module_alarm.alarm;
 
 import com.example.module_utility.response.Response;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,32 +22,33 @@ public class AlarmController {
     }
 
     @GetMapping("")
-    public ResponseEntity<List<AlarmResponseDTO>> findAlarmByUserId(@RequestParam final Integer userId) {
-        Response<List<AlarmResponseDTO>> response = alarmService.findAlarmByUserId(userId);
-        return ResponseEntity.ok(response.getData());
+    public ResponseEntity<Response<List<AlarmResponseDTO>>> findAlarmByUserId(@RequestParam final Integer userId) {
+        List<AlarmResponseDTO> response = alarmService.findAlarmByUserId(userId);
+        return ResponseEntity.ok(Response.success(response));
     }
 
     @PatchMapping("/{alarmId}")
-    public ResponseEntity updateAlarm(@PathVariable Integer alarmId) {
+    public ResponseEntity<Response> updateAlarm(@PathVariable Integer alarmId) {
         Response response = alarmService.updateAlarmReadStatus(alarmId);
         return ResponseEntity.status(response.getStatus()).body(response);
     }
 
     @PatchMapping("")
-    public ResponseEntity updateAllAlarm(@RequestParam final Integer userId) {
-        Response response = alarmService.updateAllAlarmReadStatus(userId);
-        return ResponseEntity.status(response.getStatus()).body(response);
+    public ResponseEntity<Response<Void>> updateAllAlarm(@RequestParam final Integer userId) {
+        alarmService.updateAllAlarmReadStatus(userId);
+        Response<Void> response = Response.successWithoutData();
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @DeleteMapping("")
-    public ResponseEntity deleteAllAlarm(@RequestParam final Integer userId) {
-        Response response = alarmService.deleteAllAlarm(userId);
-        return ResponseEntity.status(response.getStatus()).body(response);
+    public ResponseEntity<Response<Void>> deleteAllAlarm(@RequestParam final Integer userId) {
+        alarmService.deleteAllAlarm(userId);
+        return ResponseEntity.ok(Response.successWithoutData());
     }
 
     @DeleteMapping("/{alarmId}")
-    public ResponseEntity deleteAlarmById(@PathVariable final Integer alarmId) {
-        Response response = alarmService.deleteAlarmById(alarmId);
-        return ResponseEntity.status(response.getStatus()).body(response);
+    public ResponseEntity<Response<Void>> deleteAlarmById(@PathVariable final Integer alarmId) {
+        alarmService.deleteAlarmById(alarmId);
+        return ResponseEntity.ok(Response.successWithoutData());
     }
 }
