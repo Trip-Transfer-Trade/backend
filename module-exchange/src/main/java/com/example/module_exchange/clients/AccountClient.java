7ca8a8f45d566b19a0690525ae7e4a82a0c628b5
0321minji <0321minji@ewhain.net>
2025-03-03 18:25:30 +0900

    git commit -m "fix : modify exchange logic(#8)"

--- module-exchange/src/main/java/com/example/module_exchange/clients/AccountClient.java
+++ module-exchange/src/main/java/com/example/module_exchange/clients/AccountClient.java
@@ -1,6 +1,7 @@
 package com.example.module_exchange.clients;
 
 import com.example.module_trip.account.AccountResponseDTO;
+import com.example.module_trip.account.AccountType;
 import com.example.module_trip.account.AccountUpdateResponseDTO;
 import com.example.module_utility.response.Response;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -9,8 +10,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 
-@FeignClient(name = "trip-service", url = "http://localhost:8083/api/accounts")
+@FeignClient(name = "account-service", url = "http://localhost:8083/api/accounts")
 public interface AccountClient {
+    @GetMapping(value = "", params = "accountType")
+    AccountResponseDTO getAccountByUserIdAndAccountType(@RequestParam("userId") Integer userId, @RequestParam("accountType") AccountType accountType);
 
     @GetMapping("/{accountId}")
     AccountResponseDTO getAccountById(@PathVariable int accountId);
