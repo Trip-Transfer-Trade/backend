7495d893dce394d4baa75cebdda22b7abfa67013
0321minji <0321minji@ewhain.net>
2025-03-05 08:59:21 +0900

    Fix : remove doubled api in tripgoal controller(#8)

--- module-trip/src/main/java/com/example/module_trip/tripGoal/TripGoalController.java
+++ module-trip/src/main/java/com/example/module_trip/tripGoal/TripGoalController.java
@@ -1,10 +1,11 @@
 package com.example.module_trip.tripGoal;
 
 import com.example.module_utility.response.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/trips")
@@ -30,14 +31,9 @@ public class TripGoalController {
     }
 
     @GetMapping("/{tripId}")
-    public TripGoalResponseDTO getTripGoal(@PathVariable Integer tripId) {
-        logger.info("📌 getTripGoal 호출됨 - tripId: {}", tripId);
-        return tripGoalService.findTripGoalById(tripId);
-    }
-
-    @GetMapping("/{tripId}")
     public ResponseEntity<Response<TripGoalResponseDTO>> getTripGoal(@PathVariable Integer tripId) {
         TripGoalResponseDTO response = tripGoalService.findTripGoalById(tripId);
+        logger.info("📌 getTripGoal 호출됨 - tripId: {}", tripId);
         return ResponseEntity.ok(Response.success(response));
     }
 
