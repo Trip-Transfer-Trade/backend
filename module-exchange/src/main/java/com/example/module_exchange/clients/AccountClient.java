f591c9431053792d7b92db16992155782b78e995
0321minji <0321minji@ewhain.net>
2025-03-05 16:30:11 +0900

    git commit -m "fix : modify response using responseEntity (#8)"

--- module-exchange/src/main/java/com/example/module_exchange/clients/AccountClient.java
+++ module-exchange/src/main/java/com/example/module_exchange/clients/AccountClient.java
@@ -9,11 +9,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @FeignClient(name = "account-service", url = "http://localhost:8082/api/accounts")
 public interface AccountClient {
     @GetMapping(value = "", params = "accountType")
     AccountResponseDTO getAccountByUserIdAndAccountType(@RequestParam("userId") Integer userId, @RequestParam("accountType") AccountType accountType);
+
+    @GetMapping(value="",params="!accountType")
+    ResponseEntity<Response<List<AccountResponseDTO>>> getAccountByUserId(@RequestParam("userId") Integer userId);
 
     @GetMapping("/{accountId}")
     AccountResponseDTO getAccountById(@PathVariable int accountId);
