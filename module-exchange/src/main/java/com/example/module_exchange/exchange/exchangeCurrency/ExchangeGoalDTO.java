91cd4601be49b9b3374841965824a8173513cb65
0321minji <0321minji@ewhain.net>
2025-03-11 19:29:31 +0900

    git commit -m "feat : add dto(#132)"

---
+++ module-exchange/src/main/java/com/example/module_exchange/exchange/exchangeCurrency/ExchangeGoalDTO.java
@@ -0,0 +1,9 @@
+package com.example.module_exchange.exchange.exchangeCurrency;
+
+import lombok.Getter;
+
+@Getter
+public class ExchangeGoalDTO {
+    private Integer tripId;
+    private String toCurrency;
+}
