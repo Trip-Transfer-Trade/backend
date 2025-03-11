e1a096a10ac7cbfbc3db40b5ba70dcb6ebacb515
cie10 <leesky0075@dgu.ac.kr>
2025-03-11 13:46:59 +0900

    fix my trip api(#120)

--- module-exchange/src/main/java/com/example/module_exchange/exchange/exchangeCurrency/TripExchangeCurrencyDTO.java
+++ module-exchange/src/main/java/com/example/module_exchange/exchange/exchangeCurrency/TripExchangeCurrencyDTO.java
@@ -14,15 +14,19 @@ public class TripExchangeCurrencyDTO {
     private String country;
     private BigDecimal goalAmount;
     private LocalDate endDate;
+    private BigDecimal amount;
+    private BigDecimal amountUS;
     private BigDecimal totalAmountInKRW; // 통합 예수금
     private BigDecimal totalProfit;  //누적수익금(원화 + 달러)
 
-    public TripExchangeCurrencyDTO(Integer tripId, String name, String country, BigDecimal goalAmount, LocalDate endDate, BigDecimal totalAmountInKRW, BigDecimal totalProfit) {
+    public TripExchangeCurrencyDTO(Integer tripId, String name, String country, BigDecimal goalAmount, LocalDate endDate, BigDecimal amount, BigDecimal amountUS, BigDecimal totalAmountInKRW, BigDecimal totalProfit) {
         this.tripId = tripId;
         this.name = name;
         this.country = country;
         this.goalAmount = goalAmount;
         this.endDate = endDate;
+        this.amount = amount;
+        this.amountUS = amountUS;
         this.totalAmountInKRW = totalAmountInKRW;
         this.totalProfit= totalProfit;
     }
