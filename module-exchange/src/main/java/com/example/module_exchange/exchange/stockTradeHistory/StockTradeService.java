bfe45474c0598b5bee6d676b7fba1ac0dcce86b3
0321minji <0321minji@ewhain.net>
2025-03-13 10:40:38 +0900

    git commit -m "feat : create call goal check trigger(#104)"

--- module-exchange/src/main/java/com/example/module_exchange/exchange/stockTradeHistory/StockTradeService.java
+++ module-exchange/src/main/java/com/example/module_exchange/exchange/stockTradeHistory/StockTradeService.java
@@ -759,6 +759,7 @@ public class StockTradeService {
         return result;
     }
 
+<<<<<<< HEAD
     // 주문 가능 금액 확인
     public OrderCheckDTO getAmountCheck(int tripId) {
         int accountId = getAccountIdFromTripId(tripId);
@@ -784,6 +785,8 @@ public class StockTradeService {
         return Integer.parseInt(quantity.toString());
     }
 
+=======
+>>>>>>> 70c65b7 (git commit -m "feat : create call goal check trigger(#104)")
     public void checkGoal(){
         ExchangeRateChartDTO.ExchangeRateData rateData = exchangeRateChartService.getUSExchangeRate();
         tripClient.checkTripGoalTrigger(rateData.getRate());
