5d57330565c591b8099481644fd4f109c78fedb1
MinYeong-mkk <kmy26377538@gmail.com>
2025-03-13 09:33:31 +0900

    feat:fixTO  수정(#60)

--- module-exchange/src/main/java/com/example/module_exchange/exchange/exchangeCurrency/ExchangeCurrencyTotalDTO.java
+++ module-exchange/src/main/java/com/example/module_exchange/exchange/exchangeCurrency/ExchangeCurrencyTotalDTO.java
@@ -10,12 +10,14 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 public class ExchangeCurrencyTotalDTO {
 
+    private Integer accountId;
     private String amountNumber;
     private BigDecimal amount;
     private BigDecimal amountUS;
     private BigDecimal totalAmountInKRW;
 
-    public ExchangeCurrencyTotalDTO(String amountNumber, BigDecimal amount, BigDecimal amountUS, BigDecimal totalAmountInKRW) {
+    public ExchangeCurrencyTotalDTO(Integer accountId, String amountNumber, BigDecimal amount, BigDecimal amountUS, BigDecimal totalAmountInKRW) {
+        this.accountId = accountId;
         this.amountNumber = amountNumber;
         this.amount = amount;
         this.amountUS = amountUS;
