23a46d8691acdad76dd0ec1632f6a48513b1644e
jeongIN <shjin4667@gmail.com>
2025-03-13 11:02:53 +0900

    feat: trip goal account api (#147)

--- module-trip/src/main/java/com/example/module_trip/tripGoal/TripGoalListResponseDTO.java
+++ module-trip/src/main/java/com/example/module_trip/tripGoal/TripGoalListResponseDTO.java
@@ -1,5 +1,6 @@
 package com.example.module_trip.tripGoal;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,15 +15,17 @@ public class TripGoalListResponseDTO {
     private Integer id;
     private String name;
     private String country;
+    private String accountNumber;
     private BigDecimal goalAmount;
     private LocalDate endDate;
     private BigDecimal profit; //원화 누적 수익금
     private BigDecimal profitUs; //달러 누적 수익금
 
     @Builder
-    public TripGoalListResponseDTO(Integer id, String name, String country, BigDecimal goalAmount, LocalDate endDate, BigDecimal profit, BigDecimal profitUs) {
+    public TripGoalListResponseDTO(Integer id, String name, String accountNumber, String country, BigDecimal goalAmount, LocalDate endDate, BigDecimal profit, BigDecimal profitUs) {
         this.id = id;
         this.name = name;
+        this.accountNumber = accountNumber;
         this.country = country;
         this.goalAmount = goalAmount;
         this.endDate = endDate;
@@ -31,6 +34,8 @@ public class TripGoalListResponseDTO {
 
     }
 
+
+    @Builder
     public static TripGoalListResponseDTO toDTO(TripGoal tripGoal) {
         return TripGoalListResponseDTO.builder()
                 .id(tripGoal.getId())
@@ -42,5 +47,20 @@ public class TripGoalListResponseDTO {
                 .profitUs(tripGoal.getProfitUs())
                 .build();
     }
+
+    @Builder
+    public static TripGoalListResponseDTO toDTO(TripGoal tripGoal, String accountNumber) {
+        return TripGoalListResponseDTO.builder()
+                .id(tripGoal.getId())
+                .name(tripGoal.getName())
+                .accountNumber(accountNumber)
+                .country(tripGoal.getCountry())
+                .goalAmount(tripGoal.getGoalAmount())
+                .endDate(tripGoal.getEndDate())
+                .profit(tripGoal.getProfit())
+                .profitUs(tripGoal.getProfitUs())
+                .build();
+    }
+
 }
 
