ba2f043e7cec87a8282c2af12fa1ec9e5def8efd
cie10 <leesky0075@dgu.ac.kr>
2025-03-14 19:00:29 +0900

    edit

--- module-exchange/src/main/java/com/example/module_exchange/clients/TripClient.java
+++ module-exchange/src/main/java/com/example/module_exchange/clients/TripClient.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(name="trip-service", url="http://localhost:8082/api/trips")
+@FeignClient(name="trip-service")
 public interface TripClient {
 
     @GetMapping("/{tripId}")
