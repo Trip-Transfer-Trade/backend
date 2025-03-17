6b8ce06a8631533efba8e7668bd0c237b949698b
jeongIN <shjin4667@gmail.com>
2025-03-17 12:56:08 +0900

    edit

--- module-exchange/src/main/java/com/example/module_exchange/redisData/RestTemplateConfig.java
+++ module-exchange/src/main/java/com/example/module_exchange/redisData/RestTemplateConfig.java
@@ -16,21 +16,7 @@ import javax.net.ssl.SSLContext;
 import java.io.File;
 
 @Configuration
-public class RestTemplateConfig {package com.example.module_exchange.redisData;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.web.client.RestTemplate;
-
-    @Configuration
-    public class RestTemplateConfig {
-
-        @Bean
-        public RestTemplate restTemplate() {
-            return new RestTemplate();
-        }
-    }
+public class RestTemplateConfig {
 
     @Bean
     public RestTemplate restTemplate() throws Exception {
